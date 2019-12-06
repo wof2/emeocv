fca6632ced94c7c1d23d254c9f7667f0c981c08e
Szymon Gładysz <komw@sgladysz.com>
2019-12-06 18:27:57 +0100

    getting digits length from config instead of hardcoded value

--- Plausi.h
+++ Plausi.h
@@ -11,27 +11,29 @@
 #include <utility>
 #include <ctime>
 #include "RecognitionStats.h"
-    
+#include "Config.h"
+
 class Plausi {
 public:
-    Plausi(double maxPower = 50. /*kW*/, int window = 13);
+    Plausi(const Config& config, double maxPower = 50. /*kW*/, int window = 13);
     bool check(const std::string & value, time_t time);
     double getCheckedValue();
     time_t getCheckedTime();
 	std::string getStats();
 	void resetStats();
 	void registerNotRecognized();
-	
+
 private:
     std::string queueAsString();
-    
+
     double _maxPower;
     int _window;
     std::deque<std::pair<time_t, double> > _queue;
     time_t _time;
     double _value;
+    Config _config;
 	RecognitionStats _stats;
-	
+
 };
 
 #endif /* PLAUSI_H_ */
